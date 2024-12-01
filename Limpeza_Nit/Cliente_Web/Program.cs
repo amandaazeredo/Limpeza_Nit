c7a76c0c2f6d2731266653ef5415fd662f6d632c
Amanda Azeredo <99839997+amandaazeredo@users.noreply.github.com>
2024-12-01 14:43:43 -0300

    adicionando front web para upload de arquivos

---
+++ Limpeza_Nit/Cliente_Web/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.SignalR.Client;
+using Cliente_Web;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7118/") });
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+
+
+builder.Services.AddSingleton<HubConnection>(sp =>
+{
+    var navigationManager = sp.GetRequiredService<NavigationManager>();
+    return new HubConnectionBuilder()
+        .WithUrl(navigationManager.ToAbsoluteUri("/hub")) // URL do hub
+        .Build();
+});
+
+await builder.Build().RunAsync();
