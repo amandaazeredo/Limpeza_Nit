cacd2ea039648742b769869da90199b9cf553231
Amanda Azeredo <99839997+amandaazeredo@users.noreply.github.com>
2024-12-01 14:49:52 -0300

    commit de conexão com banco de dados

---
+++ Limpeza_Nit/API/Infra/DataAcess/Repository/ExcelImportRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using API.Infra.DataAcess.Connection;
+
+namespace API.Infra.DataAcess.Repository
+{
+    public class ExcelImportRepository(DbSession session) : IExcelImportRepository
+    {
+        private readonly DbSession _session = session;
+
+        public void BulkInsertToDatabase(DataTable dataTable, string tableName)
+        {
+            _session.Connection.DestinationTableName = tableName;
+            _session.Connection.WriteToServer(dataTable);
+        }
+    }
+}
